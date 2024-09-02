06a9a18e38780c0be264ae21c78b7fca661d15df
mateusmirandaribas <contato.mateusmribas@gmail.com>
2024-09-02 16:55:11 -0300

    feat: adding exercise one resolution

---
+++ scripts/exercise-one-script.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once '../index.php';
+
+CONST INDEX = 13;
+$k = 0;
+$result = 0;
+
+for (INDEX; $k < INDEX; $k++) {
+    $result = $result + $k;
+}
+
+echo 'O valor da variável soma é: ' . $result;
